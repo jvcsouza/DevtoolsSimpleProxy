42c91bf7188e66dd230abc69e72a3c678c2a7fa1
João <joao.souza@appsistemas.com.br>
2026-02-18 16:32:49 -0300

    Corrigido `chrome.mock-service.ts` substituindo `globalThis` em produção

--- src/services/chrome.mock-service.ts
+++ src/services/chrome.mock-service.ts
@@ -1,18 +1,19 @@
 // @ts-nocheck
-if (globalThis.chrome) void 0;
 
 import { sampleProfiles, sampleRules } from '../data/data.ts';
 
-globalThis.chrome = {};
+if (!globalThis.chrome) {
+	globalThis.chrome = {};
 
-chrome.tabs = {};
-chrome.declarativeNetRequest = {};
-chrome.storage = {
-	local: {
-		get: async key => {
-			if (key === 'rules') return { rules: sampleRules };
-			return {};
+	chrome.tabs = {};
+	chrome.declarativeNetRequest = {};
+	chrome.storage = {
+		local: {
+			get: async key => {
+				if (key === 'rules') return { rules: sampleRules };
+				return {};
+			},
+			set: async ({ rules }) => {},
 		},
-		set: async ({ rules }) => {},
-	},
-};
+	};
+}
